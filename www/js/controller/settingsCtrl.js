3003a3c5aa964d2f170692f00ee21dc9be93908a
Samuel Bach <samuel.z.bach@gmail.com>
2015-01-03 13:33:24 +0100

    introduced "All Routines" Slider

--- www/js/controller/settingsCtrl.js
+++ www/js/controller/settingsCtrl.js
@@ -9,4 +9,24 @@ app.controller('SettingsCtrl', function($scope, $mdDialog, routines) {
     };
 
     $scope.routines = routines;
+
+    $scope.allRoutinesTime = 30;
+
+    $scope.allRoutinesPause = 10;
+
+    $scope.$watch("allRoutinesTime", function(newVal, oldVal) {
+        if(newVal != oldVal) {
+            for(var i in $scope.routines) {
+                $scope.routines[i].time = newVal;
+            }
+        }
+    });
+
+    $scope.$watch("allRoutinesPause", function(newVal, oldVal) {
+        if(newVal != oldVal) {
+            for(var i in $scope.routines) {
+                $scope.routines[i].pause = newVal;
+            }
+        }
+    });
 });
